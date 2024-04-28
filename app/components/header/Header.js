53372a4c145bec4a84aed1797a25782a86843df0
umair jamaat ali <umairansari758@gmail.com>
2024-04-28 16:50:59 +0500

    header

--- app/components/header/Header.js
+++ app/components/header/Header.js
@@ -1,28 +1,13 @@
-import Image from 'next/image'
-import React from 'react'
-import img1 from '../../../public/assets/bottle.jpg'
-import ScrollAnimation from '../scrollAnimation/Scrollanimation'
+import Image from 'next/image';
+import React from 'react';
+import img1 from '../../../public/assets/bottle.jpg';
+import ScrollAnimation from '../scrollAnimation/Scrollanimation';
 
 
 export default function Header() {
     return (
         <>
-           {/* <div className="bg-white"> */}
-      {/* <div className="mx-auto w-full sm:px-6 sm:py-32 "> */}
-        <div className="relative isolate h-[740px] overflow-hidden bg-gray-100 px-6 pt-16 shadow-2xl  sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
-          {/* <svg
-            viewBox="0 0 1024 1024"
-            className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
-            aria-hidden="true"
-          >
-            <circle cx={512} cy={512} r={512} fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fillOpacity="0.7" />
-            <defs>
-              <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
-                <stop stopColor="#7775D6" />
-                <stop offset={1} stopColor="#E935C1" />
-              </radialGradient>
-            </defs>
-          </svg> */}
+        <div className="relative isolate  h-[1280px] overflow-hidden bg-gray-100 px-6 pt-16 shadow-2xl  sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
           <div className="mx-auto  max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
             <h2 className="text-3xl font-bold tracking-tight text-black sm:text-4xl">
               Full-Stack Web Design &
@@ -52,11 +37,11 @@ export default function Header() {
                 <Image width={50} height={50} src={img1} className='rounded-md'/>
             </div>
             </div>
-            <div className='absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 z-10'>
+            <div className='absolute lg:top-1/2  right-10 top-[53%] lg:left-[46%] lg:transform lg:-translate-y-1/2 lg:-translate-x-1/2 z-10'>
                 <ScrollAnimation/>
             </div>
           </div>
-          <div className="relative h-80">
+          <div className="relative  lg:h-80">
             <Image
               className="absolute left-0 bg-no-repeat top-0 w-[57rem] max-w-none bg-white/5 ring-1 ring-white/10"
               src={img1}
@@ -66,8 +51,6 @@ export default function Header() {
             />
           </div>
         </div>
-      {/* </div> */}
-    {/* </div> */}
         </>
     )
 }
--- app/components/scrollAnimation/ScrollAnimation.js
+++ app/components/scrollAnimation/ScrollAnimation.js
@@ -5,8 +5,9 @@ import img from '../../../public/assets/bottle.jpg'
 export default function ScrollAnimation() {
   return (
     <>
-      <div className="bg-green-400 rounded-full w-24 h-24 animate-spin duration-300">
-        <Image width={200} height={200} src={img}/>
+      <div className="bg-green-400 rounded-full w-24 h-24 animate-spin duration-1000">
+        {/* <Image width={200} height={200} src={img}/> */}
+        <p>hello</p>
       </div>
     </>
   );
